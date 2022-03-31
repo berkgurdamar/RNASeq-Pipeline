0c89e4397dc3a2bbf32c3cf66cb2c5d1e0e79027
berkgurdamar <berk_gurdamar@hotmail.com>
2022-03-31 11:35:49 +0300

    remove unrequired files

--- package/scripts/pathfindr_install.R
+++
@@ -1,9 +0,0 @@
-##################################################
-## Project: RNASeq-Pipeline
-## Purpose: pathfindR installing file
-## Date: September 2021
-## Author: Berk Gürdamar
-##################################################
-
-options(unzip = 'internal')
-devtools::install_github("egeulgen/pathfindR")
