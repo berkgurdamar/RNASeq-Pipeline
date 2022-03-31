289c609dca131002cc579f7a33d6e676cebb431b
berkgurdamar <berk_gurdamar@hotmail.com>
2022-03-31 21:24:31 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,13 @@
 ##################################################
 
 echo "Creating conda environment"
-conda env create -q -f env_yaml/rnaseq_pipeline.yaml
+conda env create -q -f env_yaml/rnaseq_pipeline.yml
 
 eval "$(conda shell.bash hook)"
 conda activate rnaseq-pipeline
+
+echo "Downloading multiqc"
+pip install multiqc
 
 echo "Downloading pathfindR"
 Rscript -e "devtools::install_github('egeulgen/pathfindR')"
