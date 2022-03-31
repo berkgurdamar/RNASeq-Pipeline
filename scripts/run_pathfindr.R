982d16c8c3ec1cde952a3d9eaeda427624bf8af3
berkgurdamar <berk_gurdamar@hotmail.com>
2022-03-31 21:24:33 +0300

    Update run_pathfindr.R

--- scripts/run_pathfindr.R
+++ scripts/run_pathfindr.R
@@ -6,23 +6,17 @@
 ##################################################
 
 deseq_out <- read.csv2(snakemake@input[["pathfindr_input"]])
-gene_name_converter <- read.csv2(snakemake@params[["gene_name_converter"]], sep = ",")
 
-# gene_name_converter <- read.csv2(snakemake@params[["gene_name_converter"]], row.names = 1)
-
-
+gene_name_converter <- read.csv2(snakemake@params[["gene_name_converter"]], row.names = 1)[,2:3]
+gene_name_converter <- unique(gene_name_converter)
 
 colnames(deseq_out)[1] <- colnames(gene_name_converter)[1]
 
-
 deseq_out <- deseq_out[,c(1,3,7)]
-deseq_out[,1] <- sapply(deseq_out[,1], function(x) strsplit(x, "\\.")[[1]][1])
 
-pathfindr_in <- dplyr::left_join(deseq_out, gene_name_converter, by = "Gene.stable.ID")[, c(4,2,3)]
+pathfindr_input <- dplyr::left_join(deseq_out, gene_name_converter, by = "ensembl_gene_id")[, c(4,2,3)]
 
-
-
-pathfindr_result <- pathfindR::run_pathfindR(pathfindr_in,
+pathfindr_result <- pathfindR::run_pathfindR(pathfindr_input,
                                              plot_enrichment_chart = F,
                                              visualize_enriched_terms = F,
                                              n_processes = snakemake@params[["threads"]],
